8110bffcaedb846669e41f35f9bdc2fa99325119
maguiar43 <maguiiiar@hotmail.com>
2018-03-21 14:11:26 -0300

    Versão melhorada

--- Rotina Axis Med.R
+++ Rotina Axis Med.R
@@ -1,7 +1,23 @@
 require(data.table)
+require(base)
+i=1
+dados1 <- fread("Axis2014_4.tab", sep = "\t", h = TRUE, na.strings = "")
 
-dados[i] <- 
-dados1 <- fread("Layout da base de Contas M?dicas (HRP) Colab 201705.txt", sep = "|", h = TRUE, na.strings = "")
+ano = 2016
+trimestres = 4
+
+ i=1
+ for(i in 1:trimestres){
+ oname = sprintf("dados.%d.%d", ano, i)
+ assign(oname,  fread(sprintf("Axis%d_%d.tab", ano, i),  sep = "\t", h = TRUE, na.strings = ""))
+ }
+
+
+
+ 
+ 
+ 
+
 dados2 <- fread("Layout da base de Contas M?dicas (HRP) Colab 201706.txt", sep = "|", h = TRUE, na.strings = "")
 dados3 <- fread("Layout da base de Contas M?dicas (HRP) Colab 201707.txt", sep = "|", h = TRUE, na.strings = "")
 dados4 <- fread("Layout da base de Contas M?dicas (HRP) Colab 201708.txt", sep = "|", h = TRUE, na.strings = "")
