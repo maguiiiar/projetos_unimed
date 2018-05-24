73dfd5dad2d34cbf8585bcb1b74d6ce6e14f2835
Michael Rosa Rezende <mrrezende@unimed014.com.br>
2018-05-24 17:01:00 -0300

    Atualização Rotina Demanda Espontânea.

--- Rotina Base Demanda Espontanea.R
+++ Rotina Base Demanda Espontanea.R
@@ -10,16 +10,11 @@ setwd("C:/Users/mrrezende/Documents/ProjetosUnimed/Arquivos (.txt, .csv)")
 
 demandaesp <- fread("Demanda Espontânea.csv", sep = ";",h = T)
 
-demandaesp$data <- as.Date(demandaesp$data,format = "%d/%m/%Y")
-
 demandaesp$n.cartao <- as.character(demandaesp$n.cartao)
 demandaesp$cod.atend <- as.character(demandaesp$cod.atend)
 demandaesp$cod.proced <- as.character(demandaesp$cod.proced)
-
-analise <- demandaesp %>% group_by(cod.atend,n.cartao,cod.proced,
-                                   nome.procedimento,
-                                   nome.paciente,data) %>%
-                          summarise(valor = sum(valor))
+demandaesp$nro.guia <- as.character(demandaesp$nro.guia)
+colnames(demandaesp)[5] <- "Guia.SenhaAutorizacao" 
 
 demandaesp$chave <- paste(demandaesp$n.cartao,
                           "#",demandaesp$data)
@@ -31,7 +26,13 @@ unif$Guia.DataRealizacao <- as.Date(unif$Guia.DataRealizacao,
 
 unif$chave <- paste(unif$`%NumeroCartao`,"#",unif$Guia.DataRealizacao)
 
-demandaespontanea <- left_join(demandaesp,unif, by= "chave")
+demandaespontanea <- left_join(demandaesp,unif, by= c("chave"))
 
 gc()
-  
+
+table(is.na(demandaespontanea$`%Competencia`))
+
+soma <- demandaespontanea %>% group_by(Compet.) %>% 
+  summarise(n=n(),n.na = sum(is.na(`%Competencia`)), var = n.na/n)
+
+write.csv(demandaespontanea,"uniaodemandaespont.csv")
