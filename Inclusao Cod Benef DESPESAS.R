b6cb6245a9c4f547846ef1612b3c316da4b693a5
Michael Rosa Rezende <mrrezende@unimed014.com.br>
2018-07-18 09:02:34 -0300

    Adicionando Comentários nos scripts

--- Inclusao Cod Benef DESPESAS.R
+++ Inclusao Cod Benef DESPESAS.R
@@ -9,16 +9,19 @@ setwd("C:/Users/mrrezende/Documents/")
 base_ativos <- fread("benef_ativos_dyad.csv", h=T, sep=";",
                      encoding = "UTF-8", na.strings = c("-",NA)) #ativos
 
-#para usar esta base, rodar tudo que esta relacionado a ela na inclusao do
-#cod benef da receita
+#para usar esta base, rodar tudo que esta relacionado a ela no script de
+#inclusao do cod benef da receita
 
 setwd("C:/Users/mrrezende/Documents/despesas por benef/")
+
+### despesas do dyad com tratamento da base
 
 despesas_dyad <- fread("desp_dyad_fim.txt", h=T, sep="\t",
                      encoding = "UTF-8", na.strings = c("-",NA)) 
 
 despesas_dyad <- despesas_dyad %>% select(-Competencia,-`Tipo Dependente`)
-#despesas do dyad
+
+#criar coluna "competência"
 
 despesas_dyad2 <- melt(despesas_dyad,variable.name = "Competencia", 
                        measure.vars = c("201705","201706", "201707", 
@@ -26,14 +29,28 @@ despesas_dyad2 <- melt(despesas_dyad,variable.name = "Competencia",
                                         "201711","201712", "201801",
                                         "201802","201803", "201804",
                                         "201805","201806"), na.rm = T) 
-#criar coluna "competência"
+
+#filtrando quem possui valor em 201705 e nao tem nome
 
 despesas_dyad <- despesas_dyad2 %>% filter(!is.na(`Beneficiario Codigo`))
 
+#base final para uso
+
 write.csv(despesas_dyad, file = "Despesas_dyad_introchave.csv")
+
+## rodando base tratada com chave a partir do excel e fazendo tratativas
 
 despesas_dyad <- fread("despesas_dyad_introchave.txt", h=T, sep = "\t",
                        encoding = "UTF-8", na.strings = c("-",NA))
+
+despesas_dyad$NumeroCartao <- as.character(despesas_dyad$NumeroCartao)
+
+despesas_dyad <- despesas_dyad %>% select(chave,`Beneficiario Codigo`,
+                                          `Beneficiario CNP`,
+                                          NumeroCartao)
+names(despesas_dyad)[3] <- "Cnp"
+
+### rodando base do cardio com tratamento no excel e tratamento da mesma
 
 despesas_cardio <- fread("desp_cardio_final.txt", h=T, sep = "\t",
                          encoding = "UTF-8", na.strings = c("-",NA))
@@ -51,12 +68,7 @@ despesas_cardio <- despesas_cardio %>% distinct()
 despesas_cardio <- despesas_cardio %>% select(chave,Cnp,
                                               NumeroCartao) %>% unique(.)
 
-despesas_dyad$NumeroCartao <- as.character(despesas_dyad$NumeroCartao)
-
-despesas_dyad <- despesas_dyad %>% select(chave,`Beneficiario Codigo`,
-                                          `Beneficiario CNP`,
-                                          NumeroCartao)
-names(despesas_dyad)[3] <- "Cnp"
+### inclusao de chaves para conseguir unir bases
 
 despesa_cod <- left_join(despesas_cardio,despesas_dyad[,c(2,4)], 
                          by = "NumeroCartao")
@@ -67,6 +79,8 @@ despesa_cod <- left_join(despesa_cod,despesas_dyad[,c(1,2)], by="chave",
 despesa_cod <- despesa_cod %>% filter(!is.na(Cnp))
 despesa_cod <- left_join(despesa_cod,despesas_dyad[,c(2,3)], by="Cnp",
                       suffix=c("",".cpf"))
+
+### tratamento para obter apenas uma coluna de beneficiario código
 
 despesa_cod <- despesa_cod %>% unique(.)
 
@@ -82,8 +96,11 @@ despesa_cod$`Beneficiario Codigo` <- NULL
 
 colnames(despesa_cod)[4] <- "Beneficiario Codigo"
 
-despesa_cod <- despesa_cod %>% filter(!is.na(`Beneficiario Codigo`)) 
 #limpa quem nao foi encontrado com codigo beneficiario
+
+despesa_cod <- despesa_cod %>% filter(!is.na(`Beneficiario Codigo`)) 
+
+## transforma o codigo em caracter novamente
 
 despesa_cod$`Beneficiario Codigo` <- as.character(
   despesa_cod$`Beneficiario Codigo`)
@@ -99,11 +116,17 @@ despesa_cod <- inner_join(despesa_cod,base_ativos,
 despesas_cardio <- left_join(despesas_cardio, despesa_cod, 
                              by=c("chave","Cnp","NumeroCartao"))
 
+### retira quem nao tem codigo beneficiario // talvez por nao ser ativo
+
 despesas_cardio <- despesas_cardio %>% filter(
                                             !is.na(`Beneficiario Codigo`))
 
+### limpa algumas colunas da base
+
 despesas_cardio$NumeroCartao <- NULL
 despesas_cardio$IdPessoa <- NULL
+
+### agrupa-se as despesas para somar o valor
 
 despesas_cardio <- despesas_cardio %>% group_by(Competencia,
                                               `Nome Beneficiario`,chave,
@@ -116,8 +139,13 @@ despesas_cardio <- despesas_cardio %>% group_by(Competencia,
 
 names(despesas_dyad)[5] <- "Cnp"
 despesas_dyad$NumeroCartao <- NULL
+
+## transforma o codigo em caracter novamente
+
 despesas_dyad$`Beneficiario Codigo` <- as.character(
                                       despesas_dyad$`Beneficiario Codigo`)
+
+## muda a ordem das colunas e insere novos nomes
 
 despesas_cardio <- despesas_cardio[,c(6,2,4,5,1,7,3)]
 
@@ -125,22 +153,31 @@ colnames(despesas_dyad)[2] <- "Nome Beneficiario"
 colnames(despesas_dyad)[3] <- "Beneficiario.DtNascimento"
 colnames(despesas_dyad)[6] <- "Total"
 
+## uniao das bases cardio e dyad
+
 despesa <- bind_rows(despesas_cardio,despesas_dyad)
 
+### verificando se na base só há ativos no período de interesse
+
 despesa_FINAL <- inner_join(despesa,base_ativos, by="Beneficiario Codigo")
+
+#exportação da base para ser rodada no knime com a prob de risco
 
 write.table(despesa_FINAL, "despesa_proj.txt", sep = "\t")
 
 ### VALIDACAO DE DADOS
 
 
-# juntion <- inner_join(despesas_dyad,base_ativos, 
+# juntion <- inner_join(despesas_dyad,base_ativos,
 #                       by= c("Beneficiario Codigo"))
 # 
-# testes_after_atv <- juntion %>% group_by(Competencia) %>% summarise(Total = sum(Total))
+# testes_after_atv <- juntion %>% group_by(
+#   Competencia) %>% summarise(Total = sum(Total))
 # 
-# testes_bef_atv <- despesas_dyad %>% group_by(Competencia) %>% summarise(Total = sum(Total))
+# testes_bef_atv <- despesas_dyad %>% group_by(
+#   Competencia) %>% summarise(Total = sum(Total))
 # 
 # diferencas_ativos <- testes_after_atv$Total - testes_bef_atv$Total
 # 
-# dif <- as.data.frame(cbind(testes_after_atv$Competencia,diferencas_ativos))
+# dif <- as.data.frame(cbind(testes_after_atv$Competencia,
+#                            diferencas_ativos))
--- Inclusao Cod benef Dyad em base Cardio.R
+++ Inclusao Cod benef Dyad em base Cardio.R
@@ -8,6 +8,8 @@ require(data.table)
 
 setwd("C:/Users/mrrezende/Documents/")
 
+### RODA TODAS AS BASES JÁ TRATADAS NO EXCEL PARA INCLUSAO DA CHAVE
+
 base_ativos <- fread("benef_ativos_dyad.csv", h=T, sep=";",
                      encoding = "UTF-8", na.strings = c("-",NA)) #ativos
 
@@ -39,6 +41,10 @@ base_ativos <- base_ativos %>% filter(str_detect(Contrato.GrupoEmpresa,
 #apenas produto MAIS
 
 base_ativos <- base_ativos %>% filter(Competencia == 201803)
+
+## seleciona apenas a coluna de cod benef
+
+base_ativos <- base_ativos %>% select(`Beneficiario Codigo`)
 
 #levels(as.factor(base_ativos$`Contrato Tipo Empresa`))
 
@@ -76,6 +82,8 @@ base_cod <- base_cod %>% unique(.)
 # 
 # sum(base_rec_cardio$VlrReceitas)
 
+### tratamento para obter apenas uma coluna de beneficiario código
+
 base_cod$`Beneficiario Codigo` <- ifelse(is.na(
   base_cod$`Beneficiario Codigo`),
   base_cod$`Beneficiario Codigo.chave`,
@@ -83,36 +91,52 @@ base_cod$`Beneficiario Codigo` <- ifelse(is.na(
          base_cod$`Beneficiario Codigo`,
          base_cod$`Beneficiario Codigo.cpf`))
 
+### limpa algumas colunas da base e muda nome de algumas outras colunas
+
 base_cod$`Beneficiario Codigo.chave` <- NULL
 base_cod$`Beneficiario Codigo` <- NULL
 
 colnames(base_cod)[4] <- "Beneficiario Codigo"
 
+### retira quem nao tem codigo beneficiario // talvez por nao ser ativo
+
 base_cod <- base_cod %>% filter(!is.na(`Beneficiario Codigo.cpf`))
 
-base_ativos <- base_ativos %>% select(`Beneficiario Codigo`)
+## encontra na base apenas beneficiarios ativos
 
 base_cod <- inner_join(base_cod,base_ativos, by= c("Beneficiario Codigo"))
 
 # base_receita_final <- bind_rows(base_cod,base_rec_dyad)
 
+## roda a base cardio novamente para fazer a juncao
+
 base_rec_cardio <- fread("receitacardiocpf.txt", h=T, sep = "\t",
                          na.strings = c("-",NA), colClasses = c(
                            NumeroCartao = "character")) #receitas cardio
 
+## coloca o cartao como caracter
+
 base_rec_cardio$NumeroCartao <- as.character(base_rec_cardio$NumeroCartao)
+
+### encontra-se os codigos dos beneficiarios
 
 receita_cardio <- left_join(base_rec_cardio, base_cod, by=c("chave","CNP",
                                                           "NumeroCartao"))
 
-# write.csv(receita_cardio,file = "cardiobaser.csv")
+## retira quem não possui um codigo de beneficiario do dyad
 
 receita_cardio <- receita_cardio %>% filter(!is.na(`Beneficiario Codigo`))
 
+## transforma valor em numerico
+
 receita_cardio$VlrReceitas <- as.numeric(receita_cardio$VlrReceitas)
+
+## retira algumas colunas da base
 
 receita_cardio$NumeroCartao <- NULL
 receita_cardio$IdPessoa <- NULL
+
+## soma as receitas
 
 receita_cardio <- receita_cardio %>% group_by(Competencia,
                                               `Nome Beneficiário`,chave,
@@ -121,8 +145,12 @@ receita_cardio <- receita_cardio %>% group_by(Competencia,
                                               `Beneficiario Codigo`) %>% 
                                      summarise(Total = sum(VlrReceitas))
 
+# muda o nome de algumas colunas
+
 colnames(receita_cardio)[2] <- "Nome Beneficiário"
 colnames(receita_cardio)[4] <- "Dt Nasc"
+
+## roda novamente a base do dyad de receitas
 
 base_rec_dyad <- fread("receitadyadcpf.txt", h=T, sep = "\t", 
                        na.strings = c("-",NA), colClasses = c(
@@ -130,15 +158,23 @@ base_rec_dyad <- fread("receitadyadcpf.txt", h=T, sep = "\t",
                          `Beneficiario Codigo` = "character"))
 #receitas dyad
 
+## retira algumas colunas da base
+
 base_rec_dyad$NumeroCartao <- NULL
+
+## muda a ordem das colunas na base
 
 receita_cardio <- receita_cardio[,c(6,2,3,5,4,1,7)]
 base_rec_dyad <- base_rec_dyad[,c(4,2,3,7,5,1,6)]
 
+## une as receitas das bases diferentes
+
 receita <- bind_rows(receita_cardio, base_rec_dyad)
+
+## verifica apenas beneficiarios ativos
 
 receita_FINAL <- inner_join(receita,base_ativos, by="Beneficiario Codigo")
 
-#sum(receita$Total)
+## exportação da base para o knime
 
 write.table(receita_FINAL, "receita_proje.txt", sep = "\t")
