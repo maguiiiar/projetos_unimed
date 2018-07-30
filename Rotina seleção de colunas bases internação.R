68a054c2e7ed8b73173bfdf792239452dec9862c
michaelrrezende <michaelrrezende@outlook.com>
2018-07-30 15:05:02 -0300

    Atualização final Rotina de seleção

--- Rotina seleção de colunas bases internação.R
+++ Rotina seleção de colunas bases internação.R
@@ -4,23 +4,15 @@ require(data.table)
 setwd("C:/ProjetosUnimed/Arquivos (.txt, .csv)/Joyce/")
 
 colab012018 <- fread("Colaborador_201801.txt",sep = "|",header = T)
-
 colab022018 <- fread("Colaborador_201802.txt",sep = "|",header = T)
-
 colab032018 <- fread("Colaborador_201803.txt",sep = "|",header = T)
-
 colab042018 <- fread("Colaborador_201804.txt",sep = "|",header = T)
-
 colab052018 <- fread("Colaborador_201805.txt",sep = "|",header = T)
 
 prepg012018 <- fread("PrePagamento_201801.txt",sep = "|",header = T)
-
 prepg022018 <- fread("PrePagamento_201802.txt",sep = "|",header = T)
-
 prepg032018 <- fread("PrePagamento_201803.txt",sep = "|",header = T)
-
 prepg042018 <- fread("PrePagamento_201804.txt",sep = "|",header = T)
-
 prepg052018 <- fread("PrePagamento_201805.txt",sep = "|",header = T)
 
 colab012018 <- colab012018 %>% select(-Guia.ProcedimentoClasseDyad)
