4727216c40f36fbf55c3b17a0f5033d7a47426a9
michaelrrezende <michaelrrezende@outlook.com>
2018-08-02 17:05:57 -0300

    Nova Rotina para Join classes credenciados

---
+++ Rotina Testes Ações Beneficiarios.R
@@ -0,0 +1,26 @@
+require(data.table)
+require(dplyr)
+require(caret)
+
+baserecursos <- fread("C:/Users/mrrezende/Documents/Base RP.csv", sep = ",")
+
+basesn <- fread("C:/Users/mrrezende/Documents/BaseAcao.csv", sep = ",")
+
+basesn <- basesn %>% select(-`Geom. mean(P(Internação=Sim))`,
+                            -`Geom. mean(P(Internação=Não))`,
+                            -`Geom. mean(P(Internação=Sim)+1)`,
+                            -`Geom. mean(P(Internação=Não)+1)`)
+
+colnames(baserecursos)[1] <- "Beneficiario Codigo"
+colnames(basesn)[1] <- "Beneficiario Codigo"
+
+tomadadecisao <- anti_join(basesn,baserecursos, by = "Beneficiario Codigo")
+### QUEM DEVE SER ACIONADO ###
+
+tomadadecisao2 <- inner_join(basesn,baserecursos, by = "Beneficiario Codigo")
+### QUEM ESTA DENTRO DE PROGRAMAS DE RECURSOS PROPRIOS ###
+
+jaatendidos <- tomadadecisao2 %>% group_by(`Credenciado Classe`) %>% 
+  summarise(qtde_benef = n_distinct(`Beneficiario Codigo`))
+
+fwrite(tomadadecisao, file = "BaseTomadaDeDecisão.csv", sep = "\t")
