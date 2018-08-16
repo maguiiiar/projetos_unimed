51fa4c8b463af1f876e0031689448735ce4bcf86
michaelrrezende <michaelrrezende@outlook.com>
2018-08-16 16:58:18 -0300

    Atualização Diária

--- Rotina Junção de Bases Receitas e Despesas.R
+++ Rotina Junção de Bases Receitas e Despesas.R
@@ -9,7 +9,9 @@ setwd("C:/ProjetosUnimed/Arquivos (.txt, .csv)/Base Despesas GERAL/")
 despesas <- list.files(pattern = "*.txt") %>% 
   lapply(fread,colClasses = c(`IdPessoa`="character", 
                               `CodBeneficiario`="character",
-                              `Competencia` = "character"),
+                              `Competencia` = "character",
+                              `FctEvento.QtdUtilizacaoAjustado`="numeric",
+                              `FctCusto.VlrTotalAjustado` = "numeric"),
          stringsAsFactors=F, encoding="UTF-8", sep = "|",
          select=c("Competencia","Evento.DtAbEvento",
                   "ClasseTratamentoAjustada",
@@ -17,74 +19,42 @@ despesas <- list.files(pattern = "*.txt") %>%
                   "FctCusto.VlrTotalAjustado",
                   "CodBeneficiario","NomeBeneficiario","Cnp",
                   "IdPessoa","DtNascimento","TipoEmpresa",
-                  "GrupoEmpresa"))  %>% bind_rows %>% 
-  group_by(Competencia,NomeBeneficiario,Cnp,
-           CodBeneficiario,IdPessoa,DtNascimento,TipoEmpresa) %>% 
-  summarise(qtde_util = sum(
-    FctEvento.QtdUtilizacaoAjustado),
-    valor = sum(
-      FctCusto.VlrTotalAjustado))
+                  "GrupoEmpresa"))  %>% bind_rows #%>% 
+ 
+desp.cardio <- despesas %>% group_by(Competencia,CodBeneficiario,Cnp,
+                                     NomeBeneficiario,IdPessoa,
+                                     DtNascimento,TipoEmpresa) %>% 
+                            summarise(qtde_util = sum(
+                              FctEvento.QtdUtilizacaoAjustado),
+                              valor = sum(FctCusto.VlrTotalAjustado))
 
 
-despesas$chave <- paste0(substr(despesas$NomeBeneficiario,1,13),despesas$DtNascimento, collapse = "#")
+desp.cardio$chave <- paste0(substr(desp.cardio$NomeBeneficiario,1,13),
+                            desp.cardio$DtNascimento, collapse = "#")
 
 teste <- despesas %>% filter(TipoEmpresa == "Colaborador")
 
-# despesas012015_colab <- fread("Cardio_Colaborador_201501.txt", sep = "|",encoding = "UTF-8")
-# despesas022015_colab <- fread("Cardio_Colaborador_201502.txt", sep = "|",encoding = "UTF-8")
-# despesas032015_colab <- fread("Cardio_Colaborador_201503.txt", sep = "|",encoding = "UTF-8")
-# despesas042015_colab <- fread("Cardio_Colaborador_201504.txt", sep = "|",encoding = "UTF-8")
-# despesas052015_colab <- fread("Cardio_Colaborador_201505.txt", sep = "|",encoding = "UTF-8")
-# despesas062015_colab <- fread("Cardio_Colaborador_201506.txt", sep = "|",encoding = "UTF-8")
-# despesas072015_colab <- fread("Cardio_Colaborador_201507.txt", sep = "|",encoding = "UTF-8")
-# despesas082015_colab <- fread("Cardio_Colaborador_201508.txt", sep = "|",encoding = "UTF-8")
-# despesas092015_colab <- fread("Cardio_Colaborador_201509.txt", sep = "|",encoding = "UTF-8")
-# despesas102015_colab <- fread("Cardio_Colaborador_201510.txt", sep = "|",encoding = "UTF-8")
-# despesas112015_colab <- fread("Cardio_Colaborador_201511.txt", sep = "|",encoding = "UTF-8")
-# despesas122015_colab <- fread("Cardio_Colaborador_201512.txt", sep = "|",encoding = "UTF-8")
-# despesas012016_colab <- fread("Cardio_Colaborador_201601.txt", sep = "|",encoding = "UTF-8")
-# despesas022016_colab <- fread("Cardio_Colaborador_201602.txt", sep = "|",encoding = "UTF-8")
-# despesas032016_colab <- fread("Cardio_Colaborador_201603.txt", sep = "|",encoding = "UTF-8")
-# despesas042016_colab <- fread("Cardio_Colaborador_201604.txt", sep = "|",encoding = "UTF-8")
-# despesas052016_colab <- fread("Cardio_Colaborador_201605.txt", sep = "|",encoding = "UTF-8")
-# despesas062016_colab <- fread("Cardio_Colaborador_201606.txt", sep = "|",encoding = "UTF-8")
-# despesas072016_colab <- fread("Cardio_Colaborador_201607.txt", sep = "|",encoding = "UTF-8")
-# despesas082016_colab <- fread("Cardio_Colaborador_201608.txt", sep = "|",encoding = "UTF-8")
-# despesas092016_colab <- fread("Cardio_Colaborador_201609.txt", sep = "|",encoding = "UTF-8")
-# despesas102016_colab <- fread("Cardio_Colaborador_201610.txt", sep = "|",encoding = "UTF-8")
-# despesas112016_colab <- fread("Cardio_Colaborador_201611.txt", sep = "|",encoding = "UTF-8")
-# despesas122016_colab <- fread("Cardio_Colaborador_201612.txt", sep = "|",encoding = "UTF-8")
-# despesas012017_colab <- fread("Cardio_Colaborador_201701.txt", sep = "|",encoding = "UTF-8")
-# despesas022017_colab <- fread("Cardio_Colaborador_201702.txt", sep = "|",encoding = "UTF-8")
-# despesas032017_colab <- fread("Cardio_Colaborador_201703.txt", sep = "|",encoding = "UTF-8")
-# despesas042017_colab <- fread("Cardio_Colaborador_201704.txt", sep = "|",encoding = "UTF-8")
-# 
-# 
-# despesas012015_prepg <- fread("Cardio_PrePagamento_201501.txt", sep = "|",encoding = "UTF-8")
-# despesas022015_prepg <- fread("Cardio_PrePagamento_201502.txt", sep = "|",encoding = "UTF-8")
-# despesas032015_prepg <- fread("Cardio_PrePagamento_201503.txt", sep = "|",encoding = "UTF-8")
-# despesas042015_prepg <- fread("Cardio_PrePagamento_201504.txt", sep = "|",encoding = "UTF-8")
-# despesas052015_prepg <- fread("Cardio_PrePagamento_201505.txt", sep = "|",encoding = "UTF-8")
-# despesas062015_prepg <- fread("Cardio_PrePagamento_201506.txt", sep = "|",encoding = "UTF-8")
-# despesas072015_prepg <- fread("Cardio_PrePagamento_201507.txt", sep = "|",encoding = "UTF-8")
-# despesas082015_prepg <- fread("Cardio_PrePagamento_201508.txt", sep = "|",encoding = "UTF-8")
-# despesas092015_prepg <- fread("Cardio_PrePagamento_201509.txt", sep = "|",encoding = "UTF-8")
-# despesas102015_prepg <- fread("Cardio_PrePagamento_201510.txt", sep = "|",encoding = "UTF-8")
-# despesas112015_prepg <- fread("Cardio_PrePagamento_201511.txt", sep = "|",encoding = "UTF-8")
-# despesas122015_prepg <- fread("Cardio_PrePagamento_201512.txt", sep = "|",encoding = "UTF-8")
-# despesas012016_prepg <- fread("Cardio_PrePagamento_201601.txt", sep = "|",encoding = "UTF-8")
-# despesas022016_prepg <- fread("Cardio_PrePagamento_201602.txt", sep = "|",encoding = "UTF-8")
-# despesas032016_prepg <- fread("Cardio_PrePagamento_201603.txt", sep = "|",encoding = "UTF-8")
-# despesas042016_prepg <- fread("Cardio_PrePagamento_201604.txt", sep = "|",encoding = "UTF-8")
-# despesas052016_prepg <- fread("Cardio_PrePagamento_201605.txt", sep = "|",encoding = "UTF-8")
-# despesas062016_prepg <- fread("Cardio_PrePagamento_201606.txt", sep = "|",encoding = "UTF-8")
-# despesas072016_prepg <- fread("Cardio_PrePagamento_201607.txt", sep = "|",encoding = "UTF-8")
-# despesas082016_prepg <- fread("Cardio_PrePagamento_201608.txt", sep = "|",encoding = "UTF-8")
-# despesas092016_prepg <- fread("Cardio_PrePagamento_201609.txt", sep = "|",encoding = "UTF-8")
-# despesas102016_prepg <- fread("Cardio_PrePagamento_201610.txt", sep = "|",encoding = "UTF-8")
-# despesas112016_prepg <- fread("Cardio_PrePagamento_201611.txt", sep = "|",encoding = "UTF-8")
-# despesas122016_prepg <- fread("Cardio_PrePagamento_201612.txt", sep = "|",encoding = "UTF-8")
-# despesas012017_prepg <- fread("Cardio_PrePagamento_201701.txt", sep = "|",encoding = "UTF-8")
-# despesas022017_prepg <- fread("Cardio_PrePagamento_201702.txt", sep = "|",encoding = "UTF-8")
-# despesas032017_prepg <- fread("Cardio_PrePagamento_201703.txt", sep = "|",encoding = "UTF-8")
-# despesas042017_prepg <- fread("Cardio_PrePagamento_201704.txt", sep = "|",encoding = "UTF-8")
+setwd("C:/ProjetosUnimed/Arquivos (.txt, .csv)/Bases Prob. Risco/")
+
+despesas.dyad <- list.files(pattern = "*.txt") %>% 
+  lapply(fread,colClasses = c(`Beneficiario Codigo`="character", 
+                    `Competencia`="character",
+                    `Guia.ProcedimentoQuantAutorizadaAjustado` = "numeric",
+                    `Guia.ProcedimentoVlrPagoAjustado` = "numeric"),
+         stringsAsFactors=F, encoding="UTF-8", sep = "|",
+         select=c("Beneficiario Codigo","Guia.DataEmissao",
+                  "Guia.DataSolicitacao",
+                  "Guia.CustoAssistencialNome",
+                  "Guia.ProcedimentoQuantAutorizadaAjustado",
+                  "Guia.ProcedimentoVlrPagoAjustado","%NumeroCartao",
+                  "Competencia","Beneficiario Nome","Beneficiario CNP",
+                  "Contrato Tipo Empresa",
+                  "Beneficiario Data Nascimento"))  %>% bind_rows %>% 
+  group_by(Competencia,`Beneficiario Nome`,`Beneficiario CNP`,
+           `Beneficiario Codigo`,`%NumeroCartao`,
+           `Beneficiario Data Nascimento`,`Contrato Tipo Empresa`) %>%
+  summarise(qtde_util = sum(Guia.ProcedimentoQuantAutorizadaAjustado),
+    valor = sum(Guia.ProcedimentoVlrPagoAjustado))
+
+dep.1 <- fread("Colaborador_201705.txt", sep = "|", h=T)
+
