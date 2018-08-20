e28e9f6a2bbbe5a62fd449d6bca33ffcec562e63
michaelrrezende <michaelrrezende@outlook.com>
2018-08-20 17:02:08 -0300

    Atualização Diária

--- Rotina Junção de Bases Receitas e Despesas.R
+++ Rotina Junção de Bases Receitas e Despesas.R
@@ -37,16 +37,15 @@ save(despesas, file = "despesas_cardio.RData")
 
 load(file = "despesas_cardio.RData",envir = despesas.cardio)
 
+despesas_cardio <- despesas %>% ungroup() %>% select(CodBeneficiario,
+                                                 Cnp,chave) %>% unique(.)
+
+colnames(despesas_cardio)[1] <- "NumeroCartao"
+
 setwd("C:/ProjetosUnimed/Arquivos (.txt, .csv)/Bases Prob. Risco/")
 
 despesas.dyad <- list.files(pattern = "*.txt") %>% 
-  lapply(fread,colClasses = c(`Beneficiario Codigo`="character", 
-                    `Competencia`="character",
-                    `Beneficiario CNP` = "character",
-                    `%NumeroCartao` = "character",
-                    `Guia.ProcedimentoQuantAutorizadaAjustado` = "numeric",
-                    `Guia.ProcedimentoVlrPagoAjustado` = "numeric"),
-         stringsAsFactors=F, encoding="UTF-8", sep = "|",
+  lapply(fread,stringsAsFactors=F, encoding="UTF-8", sep = "|",
          select=c("Beneficiario Codigo","Guia.DataEmissao",
                   "Guia.DataSolicitacao",
                   "Guia.CustoAssistencialNome",
@@ -54,31 +53,118 @@ despesas.dyad <- list.files(pattern = "*.txt") %>%
                   "Guia.ProcedimentoVlrPagoAjustado","%NumeroCartao",
                   "Competencia","Beneficiario Nome","Beneficiario CNP",
                   "Contrato Tipo Empresa",
-                  "Beneficiario Data Nascimento")) 
+                  "Beneficiario Data Nascimento"),
+         colClasses = c(`Beneficiario Codigo`="character", 
+                         `Competencia`="character",
+                         `Beneficiario CNP` = "character",
+                         `%NumeroCartao` = "character",
+                    `Guia.ProcedimentoQuantAutorizadaAjustado` ="numeric",
+                         `Guia.ProcedimentoVlrPagoAjustado` = "numeric"))
 
-despesas.dyad.teste <- despesas.dyad  %>% bind_rows %>% 
+despesas.dyad[[2]]$Guia.ProcedimentoQuantAutorizadaAjustado <- as.numeric(
+  despesas.dyad[[2]]$Guia.ProcedimentoQuantAutorizadaAjustado)
+
+despesas.dyad[[2]]$Guia.ProcedimentoVlrPagoAjustado <- as.numeric(
+  despesas.dyad[[2]]$Guia.ProcedimentoVlrPagoAjustado)
+
+despesas.dyad[[4]]$Guia.ProcedimentoQuantAutorizadaAjustado <- as.numeric(
+  despesas.dyad[[4]]$Guia.ProcedimentoQuantAutorizadaAjustado)
+
+despesas.dyad[[4]]$Guia.ProcedimentoVlrPagoAjustado <- as.numeric(
+  despesas.dyad[[4]]$Guia.ProcedimentoVlrPagoAjustado)
+
+despesas.dyad[[14]]$Guia.ProcedimentoQuantAutorizadaAjustado <-as.numeric(
+  despesas.dyad[[14]]$Guia.ProcedimentoQuantAutorizadaAjustado)
+
+despesas.dyad[[14]]$Guia.ProcedimentoVlrPagoAjustado <- as.numeric(
+  despesas.dyad[[14]]$Guia.ProcedimentoVlrPagoAjustado)
+
+despesas.dyad[[15]]$Guia.ProcedimentoQuantAutorizadaAjustado <-as.numeric(
+  despesas.dyad[[15]]$Guia.ProcedimentoQuantAutorizadaAjustado)
+
+despesas.dyad[[15]]$Guia.ProcedimentoVlrPagoAjustado <- as.numeric(
+  despesas.dyad[[15]]$Guia.ProcedimentoVlrPagoAjustado)
+
+despesas.dyad[[17]]$Guia.ProcedimentoQuantAutorizadaAjustado <-as.numeric(
+  despesas.dyad[[17]]$Guia.ProcedimentoQuantAutorizadaAjustado)
+
+despesas.dyad[[17]]$Guia.ProcedimentoVlrPagoAjustado <- as.numeric(
+  despesas.dyad[[17]]$Guia.ProcedimentoVlrPagoAjustado)
+
+despesas.dyad[[18]]$Guia.ProcedimentoQuantAutorizadaAjustado <-as.numeric(
+  despesas.dyad[[18]]$Guia.ProcedimentoQuantAutorizadaAjustado)
+
+despesas.dyad[[18]]$Guia.ProcedimentoVlrPagoAjustado <- as.numeric(
+  despesas.dyad[[18]]$Guia.ProcedimentoVlrPagoAjustado)
+
+despesas.dyad[[19]]$Guia.ProcedimentoQuantAutorizadaAjustado <-as.numeric(
+  despesas.dyad[[19]]$Guia.ProcedimentoQuantAutorizadaAjustado)
+
+despesas.dyad[[19]]$Guia.ProcedimentoVlrPagoAjustado <- as.numeric(
+  despesas.dyad[[19]]$Guia.ProcedimentoVlrPagoAjustado)
+
+despesas.dyad[[20]]$Guia.ProcedimentoQuantAutorizadaAjustado <-as.numeric(
+  despesas.dyad[[20]]$Guia.ProcedimentoQuantAutorizadaAjustado)
+
+despesas.dyad[[20]]$Guia.ProcedimentoVlrPagoAjustado <- as.numeric(
+  despesas.dyad[[20]]$Guia.ProcedimentoVlrPagoAjustado)
+
+despesas.dyad.group <- despesas.dyad  %>% bind_rows %>% 
   group_by(Competencia,`Beneficiario Nome`,`Beneficiario CNP`,
            `Beneficiario Codigo`,`%NumeroCartao`,
            `Beneficiario Data Nascimento`,`Contrato Tipo Empresa`) %>%
   summarise(qtde_util = sum(Guia.ProcedimentoQuantAutorizadaAjustado),
     valor = sum(Guia.ProcedimentoVlrPagoAjustado))
 
-# dep.1 <- fread("Colaborador_201705.txt", sep = "|", h=T)
-# 
-# class(dep.1$Guia.ProcedimentoQuantAutorizadaAjustado)
-# 
-# dep.1$Guia.ProcedimentoQuantAutorizadaAjustado <- as.numeric(dep.1$Guia.ProcedimentoQuantAutorizadaAjustado)
-# 
-# class(dep.1$Guia.ProcedimentoVlrPagoAjustado)
-# 
-# dep.1$Guia.ProcedimentoVlrPagoAjustado <- as.numeric(dep.1$Guia.ProcedimentoVlrPagoAjustado)
-# 
-# dep.2 <- fread("Colaborador_201706.txt", sep = "|", h=T)
-# 
-# class(dep.2$Guia.ProcedimentoQuantAutorizadaAjustado)
-# 
-# dep.2$Guia.ProcedimentoQuantAutorizadaAjustado <- as.numeric(dep.2$Guia.ProcedimentoQuantAutorizadaAjustado)
-# 
-# class(dep.2$Guia.ProcedimentoVlrPagoAjustado)
-# 
-# dep.2$Guia.ProcedimentoVlrPagoAjustado <- as.numeric(dep.2$Guia.ProcedimentoVlrPagoAjustado)
+despesas.dyad.group$`Beneficiario Nome` = toupper(
+  despesas.dyad.group$`Beneficiario Nome`)
+
+despesas.dyad.group$chave <- paste0(substr(
+  despesas.dyad.group$`Beneficiario Nome`,1,13),"#",
+  despesas.dyad.group$`Beneficiario Data Nascimento`)
+
+despesas.dyad.group$`Beneficiario CNP` <- str_replace_all(
+  despesas.dyad.group$`Beneficiario CNP`, "\\.","")
+
+despesas.dyad.group$`Beneficiario CNP` <- str_replace_all(
+  despesas.dyad.group$`Beneficiario CNP`, "-","")
+
+names(despesas.dyad.group)
+names(despesas)
+
+colnames(despesas.dyad.group)[5] <- "NumeroCartao"
+
+despesas_dyad <- despesas.dyad.group %>% ungroup() %>% select(
+  `NumeroCartao`,`Beneficiario CNP`,`Beneficiario Codigo`,
+  chave) %>% unique(.)
+
+colnames(despesas_dyad)[2] <- "Cnp"
+
+despesas_union <- left_join(despesas_cardio,despesas_dyad[,c(1,3)], 
+                         by = "NumeroCartao")
+
+despesas_union <- left_join(despesas_union,despesas_dyad[,c(3,4)],
+                          by="chave", suffix=c("",".chave"))
+
+despesas_union  <- despesas_union %>% filter(!is.na(Cnp))
+despesas_union <- left_join(despesas_union,despesas_dyad[,c(2,3)], 
+                            by="Cnp",suffix=c("",".cpf"))
+
+despesas_union <- despesas_union %>% distinct()
+
+despesas_union$`Beneficiario Codigo` <- ifelse(is.na(
+  despesas_union$`Beneficiario Codigo`),
+  despesas_union$`Beneficiario Codigo.chave`,
+  ifelse(is.na(despesas_union$`Beneficiario Codigo.cpf`),
+         despesas_union$`Beneficiario Codigo`,
+         despesas_union$`Beneficiario Codigo.cpf`))
+
+despesas_union$`Beneficiario Codigo.chave` <- NULL
+despesas_union$`Beneficiario Codigo` <- NULL
+
+colnames(despesas_union)[4] <- "Beneficiario Codigo"
+
+#limpa quem nao foi encontrado com codigo beneficiario
+
+despesas_union <- despesas_union %>% filter(
+  !is.na(`Beneficiario Codigo`)) 
