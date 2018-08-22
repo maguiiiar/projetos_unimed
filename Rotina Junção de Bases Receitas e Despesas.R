72a00ec02b0ff433e53d9a80241ef7f88f05cb6f
michaelrrezende <michaelrrezende@outlook.com>
2018-08-22 11:28:37 -0300

    Atualização de base diária | agora com comentários em cada passo

--- Rotina Junção de Bases Receitas e Despesas.R
+++ Rotina Junção de Bases Receitas e Despesas.R
@@ -4,7 +4,11 @@ require(stringr)
 require(caret)
 require(data.table)
 
+### MUDANDO DIRETÓRIO PARA DESPESAS DO CARDIO
+
 setwd("C:/ProjetosUnimed/Arquivos (.txt, .csv)/Base Despesas GERAL/")
+
+### BUSCANDO ARQUIVOS, SELECIONANDO COLUNAS, UNINDO ARQUIVOS E AGRUPANDO
 
 despesas <- list.files(pattern = "*.txt") %>% 
   lapply(fread,colClasses = c(`IdPessoa`="character", 
@@ -28,23 +32,41 @@ despesas <- list.files(pattern = "*.txt") %>%
                               FctEvento.QtdUtilizacaoAjustado),
                               valor = sum(FctCusto.VlrTotalAjustado))
 
+### CRIANDO CHAVE COM NOME E DATA DE NASCIMENTO
+
 despesas$chave <- paste0(substr(despesas$NomeBeneficiario,1,13),"#",
                             despesas$DtNascimento)
 
+### MUDANDO DIRETORIO PARA SALVAR BASE EM .RDATA
+
 setwd("C:/ProjetosUnimed/Arquivos (.txt, .csv)/Bases R/")
+
+### SALVANDO BASE
 
 save(despesas, file = "despesas_cardio.RData")
 
+### CARREGANDO BASE JÁ PRONTA
+
 load(file = "despesas_cardio.RData")
 
+### MUDANDO NOME EM COLUNA DA BASE
+
 colnames(despesas)[2] <- "NumeroCartao"
+
+### SELECIONANDO APENAS COLUNAS PARA INCLUIR COD BENEFICIARIO DO DYAD
 
 despesas_cardio <- despesas %>% ungroup() %>% select(CodBeneficiario,
                                                  Cnp,chave) %>% unique(.)
 
+### MUDANDO NOME EM COLUNA DA BASE
+
 colnames(despesas_cardio)[1] <- "NumeroCartao"
 
+### MUDANDO DIRETÓRIO PARA BASES DE DESPESAS DO DYAD
+
 setwd("C:/ProjetosUnimed/Arquivos (.txt, .csv)/Bases Prob. Risco/")
+
+### LENDO ARQUIVOS E SELECIONANDO COLUNAS NECESSÁRIAS
 
 despesas.dyad <- list.files(pattern = "*.txt") %>% 
   lapply(fread,stringsAsFactors=F, encoding="UTF-8", sep = "|",
@@ -63,6 +85,8 @@ despesas.dyad <- list.files(pattern = "*.txt") %>%
                     `Guia.ProcedimentoQuantAutorizadaAjustado` ="numeric",
                          `Guia.ProcedimentoVlrPagoAjustado` = "numeric"),
          na.strings=c("","NA"))
+
+### TRANSFORMANDO COLUNAS EM NÚMERO
 
 despesas.dyad[[2]]$Guia.ProcedimentoQuantAutorizadaAjustado <- as.numeric(
   despesas.dyad[[2]]$Guia.ProcedimentoQuantAutorizadaAjustado)
@@ -112,6 +136,8 @@ despesas.dyad[[20]]$Guia.ProcedimentoQuantAutorizadaAjustado <-as.numeric(
 despesas.dyad[[20]]$Guia.ProcedimentoVlrPagoAjustado <- as.numeric(
   despesas.dyad[[20]]$Guia.ProcedimentoVlrPagoAjustado)
 
+### AGRUPANDO E SELECIONANDO COLUNAS NECESSÁRIAS
+
 despesas.dyad.group <- despesas.dyad  %>% bind_rows %>% 
   group_by(Competencia,`Beneficiario Nome`,`Beneficiario CNP`,
            `Beneficiario Codigo`,`%NumeroCartao`,
@@ -119,12 +145,18 @@ despesas.dyad.group <- despesas.dyad  %>% bind_rows %>%
   summarise(qtde_util = sum(Guia.ProcedimentoQuantAutorizadaAjustado),
     valor = sum(Guia.ProcedimentoVlrPagoAjustado))
 
+### COLOCANDO O NOME DO BENEFICIÁRIO EM MAIÚSCULO
+
 despesas.dyad.group$`Beneficiario Nome` = toupper(
   despesas.dyad.group$`Beneficiario Nome`)
+
+### CRIANDO CHAVE COM NOME E DATA DE NASCIMENTO
 
 despesas.dyad.group$chave <- paste0(substr(
   despesas.dyad.group$`Beneficiario Nome`,1,13),"#",
   despesas.dyad.group$`Beneficiario Data Nascimento`)
+
+### TRATANDO A COLUNA CPF PARA SE TER UMA OUTRA CHAVE CONSISTENTE
 
 despesas.dyad.group$`Beneficiario CNP` <- str_replace_all(
   despesas.dyad.group$`Beneficiario CNP`, "\\.","")
@@ -135,17 +167,29 @@ despesas.dyad.group$`Beneficiario CNP` <- str_replace_all(
 names(despesas.dyad.group)
 names(despesas)
 
+### MUDANDO NOME EM COLUNA DA BASE
+
 colnames(despesas.dyad.group)[5] <- "NumeroCartao"
+
+### SELECIONANDO APENAS COLUNAS NECESSÁRIAS
 
 despesas_dyad <- despesas.dyad.group %>% ungroup() %>% select(
   `NumeroCartao`,`Beneficiario CNP`,`Beneficiario Codigo`,
   chave) %>% unique(.)
 
+### MUDANDO NOME EM COLUNA DA BASE
+
 colnames(despesas_dyad)[2] <- "Cnp"
+
+### RETIRANDO QUEM SÓ POSSUI RECEITA SEM QUALQUER CÓDIGO
 
 despesas_dyad <- despesas_dyad %>% filter(!chave == "#")
 
+### TRATANDO COLUNA DO CARTÃO PARA MAIS UMA COLUNA CONSISTENTE
+
 despesas_dyad <- despesas_dyad %>% filter(nchar(NumeroCartao) <= 15)
+
+### INCLUSÃO DE CHAVES PARA ENCONTRAR OS CODIGOS DE BENEFICIARIO NO CARDIO
 
 despesas_union <- left_join(despesas_cardio,despesas_dyad[,c(1,3)], 
                          by = "NumeroCartao")
@@ -157,7 +201,11 @@ despesas_union  <- despesas_union %>% filter(!Cnp == "")
 despesas_union <- left_join(despesas_union,despesas_dyad[,c(2,3)], 
                             by="Cnp",suffix=c("",".cpf"))
 
+### TIRANDO REGISTROS DUPLICADOS
+
 despesas_union <- despesas_union %>% distinct()
+
+### UNIFICANDO OS CODIGOS EM APENAS UMA COLUNA
 
 despesas_union$`Beneficiario Codigo` <- ifelse(is.na(
   despesas_union$`Beneficiario Codigo`),
@@ -169,26 +217,32 @@ despesas_union$`Beneficiario Codigo` <- ifelse(is.na(
 despesas_union$`Beneficiario Codigo.chave` <- NULL
 despesas_union$`Beneficiario Codigo` <- NULL
 
+### MUDANDO NOME EM COLUNA DA BASE
+
 colnames(despesas_union)[4] <- "Beneficiario Codigo"
 
-#limpa quem nao foi encontrado com codigo beneficiario
+### LIMPA QUEM NAO FOI ENCONTRADO COM CODIGO BENEFICIARIO
 
 despesas_union <- despesas_union %>% filter(
-  !is.na(`Beneficiario Codigo`)) 
+  !is.na(`Beneficiario Codigo`))
 
-despesas_union <- despesas_union %>% select(-NumeroCartao) %>% distinct()
-
-############################# TESTES ####################
+### COLOCANDO OS CÓDIGOS NA BASE COM TODAS AS COLUNAS DO CARDIO
 
 despesas_cardio_final <- left_join(despesas, despesas_union, 
                              by=c("chave","Cnp","NumeroCartao"))
 
-despesas_cardio_final <- despesas_cardio_final %>% filter(
-                                            !is.na(`Beneficiario Codigo`))
+### RETIRANDO BENEFICIARIOS QUE NÃO POSSUEM CODIGO DO DYAD
+### TAMBÉM RETIRA UMA COLUNA DA BASE
 
-despesas_cardio_final$IdPessoa <- NULL
+despesas_cardio_final <- despesas_cardio_final %>% filter(!is.na(
+                                              `Beneficiario Codigo`)) %>%
+                                                select(-IdPessoa)
+
+### RETIRA REGISTROS SEM INFORMAÇÕES DE BENEFICIÁRIO
 
 despesas.dyad.group <- despesas.dyad.group %>% filter(chave != "#")
+
+### REORGANIZANDO AS COLUNAS PARA FAZER A JUNÇÃO | MUDA NOMES DE COLUNAS
 
 despesas_cardio_final <- despesas_cardio_final[,c(1,4,3,10,2,5,6,7,8,9)]
 
@@ -197,7 +251,10 @@ colnames(despesas_cardio_final)[3] <- "Beneficiario CNP"
 colnames(despesas_cardio_final)[6] <- "Beneficiario Data Nascimento"
 colnames(despesas_cardio_final)[7] <- "Contrato Tipo Empresa"
 
+### JUNÇÃO DE BASES DO CARDIO E DO DYAD
+
 despesas.final <- bind_rows(despesas_cardio_final, despesas.dyad.group)
 
-despesas.final <- despesas.final %>% filter(!is.na(valor))
+### RETIRA QUEM NÃO POSSUI VALOR DA BASE
 
+despesas.final <- despesas.final %>% filter(!is.na(valor))
