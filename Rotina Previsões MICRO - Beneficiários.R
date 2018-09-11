54e70b9a2814efc22b9ba6fd262e41a01252420d
michaelrrezende <michaelrrezende@outlook.com>
2018-09-11 16:44:52 -0300

    Atualização diária de rotinas.

---
+++ Rotina Previsões MICRO - Beneficiários.R
@@ -0,0 +1,56 @@
+require(dplyr)
+require(psych)
+require(stringr)
+require(caret)
+require(data.table)
+
+ativos <- fread("C:/ProjetosUnimed/Arquivos (.txt, .csv)/Base Benef Ativos/
+                ivos_201805.tab",
+                colClasses = c(`Beneficiario Codigo` = "character"), 
+                encoding = "UTF-8")
+
+ativos <- ativos %>% filter(`Contrato Tipo Empresa` %in% c(
+                               "Pré Pagamento", "Colaborador")) %>% select(
+                                  `Beneficiario Codigo`) %>% filter(
+                                    `Beneficiario Codigo` != "") 
+
+ativos <- ativos %>% distinct()
+
+despe.ativos <- inner_join(ativos,despesas.final,by="Beneficiario Codigo")
+
+recei.ativos <- inner_join(ativos,receitas.final,by="Beneficiario Codigo")
+
+kmeans <- fread("C:/ProjetosUnimed/Arquivos (.txt, .csv)/kmeans.txt",
+                colClasses = c(`Beneficiario Codigo` = "character"),
+                encoding = "UTF-8")
+
+kmeans <- kmeans %>% select(`Beneficiario Codigo`,Cluster)
+
+kplusdesp <- left_join(despe.ativos,kmeans, by = "Beneficiario Codigo")
+
+kplusrece <- left_join(recei.ativos,kmeans, by = "Beneficiario Codigo")
+
+kplusdesp <- kplusdesp %>% group_by(Competencia, Cluster) %>% summarise(
+  n_benef = n_distinct(`Beneficiario Codigo`), valor = sum(valor))
+
+kplusrece <- kplusrece %>% group_by(Competencia, Cluster) %>% summarise(
+  n_benef = n_distinct(`Beneficiario Codigo`), valor = sum(Valor))
+
+### MUDANDO DIRETORIO PARA SALVAR BASE EM .RDATA
+
+setwd("C:/ProjetosUnimed/Arquivos (.txt, .csv)/Bases R/")
+
+### SALVANDO BASE
+
+save(kplusdesp, file = "despesaprev.RData")
+save(kplusrece, file = "receitaprev.RData")
+
+load("despesaprev.RData")
+load("receitaprev.RData")
+
+#### EXPORTANDO BASES PARA ANÁLISE
+
+setwd("C:/Users/mrrezende/Documents/")
+
+fwrite(kplusdesp, file = "despesaprevisao", sep = ";")
+fwrite(kplusrece, file = "receitaprevisao", sep = ";")
